2ba42f5fca3e55de80eb7ed0b325aa871f0ff359
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-03-05 11:09:45 +0530

    feat: Report scheduler heartbeat to system status cache.

--- app/services/signal/scheduler.rb
+++ app/services/signal/scheduler.rb
@@ -46,6 +46,9 @@ module Signal
           break unless @running
 
           begin
+            # Report heartbeat even if market is closed to show service is alive
+            Live::SystemStatusCache.instance.report_heartbeat(:scheduler)
+
             # Early exit if market is closed - avoid unnecessary processing
             if TradingSession::Service.market_closed?
               Rails.logger.debug('[SignalScheduler] Market closed - skipping cycle (no index loading)')
