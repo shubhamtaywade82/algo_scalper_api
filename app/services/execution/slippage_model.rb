86deb8a8b7ab53a43d4542bd9636b720a27816ed
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-03-18 10:36:08 +0530

    Add SlippageModel for realistic execution

---
+++ app/services/execution/slippage_model.rb
@@ -0,0 +1,13 @@
+module Execution
+  class SlippageModel
+    def self.apply(price:, side:)
+      slippage = 0.001 # 0.1%
+
+      if side == :buy
+        price * (1 + slippage)
+      else
+        price * (1 - slippage)
+      end
+    end
+  end
+end
