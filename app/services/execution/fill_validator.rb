a27b50a70f2c3a00c22a75a2009b3729d9e75bfb
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-03-18 10:37:55 +0530

    Add FillValidator for execution correctness

---
+++ app/services/execution/fill_validator.rb
@@ -0,0 +1,11 @@
+module Execution
+  class FillValidator
+    def self.valid?(order_response)
+      return false if order_response.nil?
+      return false unless order_response[:status] == "success"
+      return false if order_response[:filled_quantity].to_i <= 0
+
+      true
+    end
+  end
+end
