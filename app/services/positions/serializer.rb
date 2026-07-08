d6fb8d4e900689e2644f55cd14f2d062175675fe
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-07-08 14:15:18 +0530

    feat: add Positions::Serializer.detail for single-position view

--- app/services/positions/serializer.rb
+++ app/services/positions/serializer.rb
@@ -49,6 +49,72 @@ module Positions
       )
     end
 
+    def detail(tracker)
+      base = tracker.exited_at.present? ? closed(tracker) : open(tracker)
+
+      base.merge(
+        entry_context: entry_context(tracker),
+        exit_block: exit_block(tracker),
+        trailing_state: trailing_state(tracker),
+        config_snapshot: tracker.meta_snapshot&.config_snapshot&.deep_symbolize_keys,
+        config_version_hash: tracker.meta_snapshot&.config_version_hash,
+        strategy_signal: strategy_signal_block(tracker),
+        meta: tracker.meta
+      )
+    end
+
+    def entry_context(tracker)
+      {
+        iv_at_entry: tracker.iv_at_entry&.to_f,
+        vix_at_entry: tracker.vix_at_entry&.to_f,
+        dte_at_entry: tracker.dte_at_entry,
+        atm_strike: tracker.atm_strike&.to_f,
+        expiry_date: tracker.expiry_date&.iso8601,
+        entry_underlying_price: tracker.entry_underlying_price&.to_f,
+        entry_tf: tracker.entry_tf,
+        alpha_source: tracker.alpha_source,
+        entry_path: tracker.entry_path,
+        signal_confidence: tracker.signal_confidence&.to_f
+      }
+    end
+
+    def exit_block(tracker)
+      return nil unless tracker.exited_at.present?
+
+      execution_meta = tracker.execution.is_a?(Hash) ? tracker.execution : {}
+      {
+        exit_reason: tracker.exit_reason,
+        exit_path: tracker.exit_path,
+        exit_classification: execution_meta['classified_as'],
+        exited_at: tracker.exited_at&.iso8601
+      }
+    end
+
+    def trailing_state(tracker)
+      {
+        high_water_mark_pnl: tracker.high_water_mark_pnl&.to_f,
+        hwm_pnl_pct: tracker.hwm_pnl_pct&.to_f,
+        secured_sl_price: tracker.secured_sl_price&.to_f,
+        breakeven_locked: tracker.breakeven_locked,
+        profit_zone_state: tracker.profit_zone_state
+      }
+    end
+
+    def strategy_signal_block(tracker)
+      signal = Strategies::Signal.find_by(position_tracker_id: tracker.id)
+      return nil unless signal
+
+      {
+        strategy_slug: signal.strategy_record.slug,
+        action: signal.action,
+        confidence: signal.confidence&.to_f,
+        outcome: signal.outcome,
+        reason: signal.reason,
+        entry_result_reason: signal.metadata['entry_result_reason'],
+        guard_results: signal.metadata['guard_results']
+      }
+    end
+
     def base_attributes(tracker)
       {
         id: tracker.id,
